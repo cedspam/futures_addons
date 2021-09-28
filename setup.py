4fed0f6c5a9fcd9648861c039b5f8cd4af29df08
cedspam <cedric.lacrambe@gmail.com>
2021-09-28 15:21:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 "module setup script"
 import setuptools
 
-with open("README.md", "r"', encoding="utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
